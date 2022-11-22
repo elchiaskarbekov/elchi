232a80cfe0e9e5e4a542266cf0a15af75e34f00c
Your Name <you@example.com>
2022-11-23 01:33:48 +0600

    add ExpenseItem component and Expensedate components

---
+++ src/App.js
@@ -0,0 +1,45 @@
+
+
+import ExpenseItem from './components/expenseItem';
+
+const App = () => {
+  // data from Backend
+  const expenses = [
+    {
+      id: 'el1',
+      title: 'Car Insueanse',
+      date: new Date(2022, 1, 25),
+      price: 200,
+    },
+    {
+      id: 'el2',
+      title: 'Tualet paper',
+      date: new Date(2022, 1, 25),
+      price: 5,
+    },
+    {
+      id: 'el3',
+      title: 'Course',
+      date: new Date(2021, 5, 15),
+      price: 180,
+    },
+    {
+      id: 'el4',
+      title: 'Utilities',
+      date: new Date(2022, 6, 30),
+      price: 100,
+    },
+  ]
+
+
+  return (
+    <div>
+     <ExpenseItem items={expenses[0]}/>
+     <ExpenseItem items={expenses[1]}/>
+     <ExpenseItem items={expenses[2]}/>
+     <ExpenseItem items={expenses[3]}/>
+    </div>
+  );
+}
+
+export default App;
